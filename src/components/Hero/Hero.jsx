39e8dbc64d18b9db27f8feb704b6064ff2da23fe
Jonathan Chen <jgchen@sas.upenn.edu>
2021-08-17 16:49:29 -0400

    change default on hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -26,7 +26,7 @@ const Header = () => {
       <Container>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
           <h1 className="hero-title">
-            {title} <span className="text-color-main">{name || 'Your Name'}</span>
+            {title} <span className="text-color-main">{name || 'Jonathan Chen'}</span>
             <br />
             {subtitle}
           </h1>
